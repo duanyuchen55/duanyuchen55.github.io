812b3ef8969a986ea0cbf8ef626132c232ff7c32
duanyuchen55 <duanyuchen55@gmail.com>
2021-01-24 18:19:35 +0800

    add gitalk2

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"/2018/06/01/header-image.html"},{'title':"Welcome",'url':"/2018/07/01/welcome.html"},{'title':123,'url':"/2021/01/23/baseline.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"/2018/06/01/header-image.html"},{'title':"Welcome",'url':"/2018/07/01/welcome.html"},{'title':"baseline",'url':"/2021/01/23/baseline.html"},{'title':"test",'url':"/2021/01/24/test.html"}]};
